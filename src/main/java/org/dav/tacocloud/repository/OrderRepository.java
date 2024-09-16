ba4cf8a8999c38a1c2805f54f902fd723621b457
ADolodarenko <adolodar@gmail.com>
2024-09-16 23:07:09 +0300

    1. Substituted Spring Data JDBC with Spring Data JPA. 2. Fixed tests.

--- src/main/java/org/dav/tacocloud/repository/OrderRepository.java
+++ src/main/java/org/dav/tacocloud/repository/OrderRepository.java
@@ -3,5 +3,8 @@ package org.dav.tacocloud.repository;
 import org.dav.tacocloud.domain.TacoOrder;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
+    List<TacoOrder> findByDeliveryZip(String deliveryZip);
 }
--- src/test/java/org/dav/tacocloud/HomeControllerTest.java
+++ src/test/java/org/dav/tacocloud/HomeControllerTest.java
@@ -6,21 +6,28 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.hamcrest.Matchers.containsString;
 
+import org.dav.tacocloud.repository.IngredientRepository;
+import org.dav.tacocloud.repository.OrderRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest
 public class HomeControllerTest {
     @Autowired
     private MockMvc mockMvc;
+    @MockBean
+    private IngredientRepository ingredientRepository;
+    @MockBean
+    private OrderRepository orderRepository;
 
     @Test
     public void testHomePage() throws Exception {
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("home"))
-                .andExpect(content().string(containsString("Welcome to...")));
+                .andExpect(content().string(containsString("Welcome to fucking")));
     }
 }
