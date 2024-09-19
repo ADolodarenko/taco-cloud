f0d3687520d91843772cfe5144433d427da771be
ADolodarenko <adolodar@gmail.com>
2024-09-19 22:55:34 +0300

    Moving to the Cassandra DBMS (hasn't done it yet).

--- src/main/java/org/dav/tacocloud/domain/TacoOrder.java
+++ src/main/java/org/dav/tacocloud/domain/TacoOrder.java
@@ -1,30 +1,29 @@
 package org.dav.tacocloud.domain;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import com.datastax.oss.driver.api.core.uuid.Uuids;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import org.dav.tacocloud.util.UDTUtils;
 import org.hibernate.validator.constraints.CreditCardNumber;
+import org.springframework.data.cassandra.core.mapping.Column;
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+import org.springframework.data.cassandra.core.mapping.Table;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @Data
-@Entity
+@Table("orders")
 public class TacoOrder {
     private static final long serialVersionUID = 1L;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    @PrimaryKey
+    private UUID id = Uuids.timeBased();
     private Date placedAt = new Date();
 
     @NotBlank(message = "Delivery name is required")
@@ -52,10 +51,10 @@ public class TacoOrder {
     @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
     private String ccCVV;
 
-    @OneToMany(cascade = CascadeType.ALL)
-    private List<Taco> tacos = new ArrayList<>();
+    @Column("tacos")
+    private List<TacoUDT> tacos = new ArrayList<>();
 
     public void addTaco(Taco taco) {
-        this.tacos.add(taco);
+        this.tacos.add(UDTUtils.toTacoUDT(taco));
     }
 }
