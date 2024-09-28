8e05d81766c1ea77fe0cfb2c57008297429e44de
ADolodarenko <adolodar@gmail.com>
2024-09-29 01:31:17 +0300

    1. Moved to MongoDB; 2. Added Spring Security - there are errors to fix.

--- src/main/java/org/dav/tacocloud/domain/TacoOrder.java
+++ src/main/java/org/dav/tacocloud/domain/TacoOrder.java
@@ -1,29 +1,26 @@
 package org.dav.tacocloud.domain;
 
-import com.datastax.oss.driver.api.core.uuid.Uuids;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
-import org.dav.tacocloud.util.UDTUtils;
 import org.hibernate.validator.constraints.CreditCardNumber;
-import org.springframework.data.cassandra.core.mapping.Column;
-import org.springframework.data.cassandra.core.mapping.PrimaryKey;
-import org.springframework.data.cassandra.core.mapping.Table;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.UUID;
 
 @Data
-@Table("orders")
-public class TacoOrder {
+@Document(collection = "orders")
+public class TacoOrder implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    @PrimaryKey
-    private UUID id = Uuids.timeBased();
+    @Id
+    private String id;
     private Date placedAt = new Date();
 
     @NotBlank(message = "Delivery name is required")
@@ -51,10 +48,9 @@ public class TacoOrder {
     @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
     private String ccCVV;
 
-    @Column("tacos")
-    private List<TacoUDT> tacos = new ArrayList<>();
+    private List<Taco> tacos = new ArrayList<>();
 
     public void addTaco(Taco taco) {
-        this.tacos.add(UDTUtils.toTacoUDT(taco));
+        this.tacos.add(taco);
     }
 }
--- src/main/java/org/dav/tacocloud/repository/OrderRepository.java
+++ src/main/java/org/dav/tacocloud/repository/OrderRepository.java
@@ -3,9 +3,5 @@ package org.dav.tacocloud.repository;
 import org.dav.tacocloud.domain.TacoOrder;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-import java.util.UUID;
-
-public interface OrderRepository extends CrudRepository<TacoOrder, UUID> {
-    List<TacoOrder> findByDeliveryZip(String deliveryZip);
+public interface OrderRepository extends CrudRepository<TacoOrder, String> {
 }
