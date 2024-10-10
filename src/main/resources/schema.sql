567a049f6d5b687655d52b190367e04b19f4784e
ADolodarenko <adolodar@gmail.com>
2024-10-10 23:50:02 +0300

    Fixed database schema. Need to proceed with bugs fixes.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,5 @@
-create table if not exists Taco_Order (
-    id identity,
+create table if not exists taco_order (
+    id bigserial not null primary key,
     delivery_name varchar(50) not null,
     delivery_street varchar(50) not null,
     delivery_city varchar(50) not null,
@@ -10,23 +10,27 @@ create table if not exists Taco_Order (
     cc_cvv varchar(3) not null,
     placed_at timestamp not null);
 
-create table if not exists Taco (
-    id identity,
-    name varchar(50) not null,
-    taco_order bigint not null,
-    taco_order_key bigint not null,
+create table if not exists taco (
+    id bigserial not null primary key,
+    "name" varchar(50) not null,
+    taco_order_id bigint not null,
     created_at timestamp not null);
 
-create table if not exists Ingredient_Ref (
-    ingredient varchar(4) not null,
-    taco bigint not null,
-    taco_key bigint not null);
+alter table taco add foreign key (taco_order_id) references taco_order(id);
 
-create table if not exists Ingredient (
+create table if not exists ingredient (
     id varchar(4) not null primary key,
-    name varchar(25) not null,
-    type varchar(10) not null);
+    "name" varchar(25) not null,
+    "type" varchar(10) not null);
 
-alter table Taco add foreign key (taco_order) references Taco_Order(id);
+create table if not exists taco_ingredient (
+    ingredient_id varchar(4) not null,
+    taco_id bigint not null);
 
-alter table Ingredient_Ref add foreign key (ingredient) references Ingredient(id);
+alter table taco_ingredient add foreign key (ingredient_id) references ingredient(id);
+alter table taco_ingredient add foreign key (taco_id) references taco(id);
+
+
+
+
+
